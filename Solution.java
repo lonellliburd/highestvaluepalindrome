5f8a899f8144a0cfd1a36bd83f4a0273a528d5af
Lonell <lonell.liburd@gmail.com>
2018-09-20 22:57:44 -0400

    Done

---
+++ Solution.java
@@ -0,0 +1,59 @@
+static String highestValuePalindrome(String s, int n, int k) {
+        int lo = 0;
+        int hi = n-1;;
+        char[] string = s.toCharArray();
+        int diff = 0;
+
+        for(int i=0, j=n-1; i<n/2; i++, j--){
+            if (string[i] != string[j]){
+                diff++;
+            }
+        }
+
+        if (diff > k){
+            return "-1";
+        }
+
+        while(hi >= lo){
+            if (k <= 0){
+                break;
+            }
+
+            if (string[lo] == string[hi]){
+                if (k > 1 && (k-2) >= diff && string[lo] != '9'){
+                    string[lo] = '9';
+                    string[hi] = '9';
+                    k-=2;
+                }
+            }
+            else {
+                if (k > 1 && (k-2) >= diff-1){
+                    if (string[lo] != '9'){
+                        string[lo] = '9';
+                        k--;
+                    }
+                    if (string[hi] != '9'){
+                        string[hi] = '9';
+                        k--;
+                    }
+                } else {
+                    if (string[lo] > string[hi]){
+                        string[hi] = string[lo];
+                    } else {
+                        string[lo] = string[hi];
+                    }
+                    k--;
+                }
+                diff--;
+            }
+            if (lo == hi && k > 0){
+                string[lo] = '9';
+                k--;
+            }
+            lo++;
+            hi--;
+        }
+
+        s = String.valueOf(string);
+        return isPalindrome(s) ? s : "-1";
+    }
